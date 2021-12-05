9d703026e9514ecdef7a756f10800ec82b03bf61
veronikasgit <w_e_r_o_n_i_k_a@mail.ru>
2021-12-06 01:45:19 +0300

    First commit

---
+++ app/src/main/java/com/company/srs7style/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.company.srs7style
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
